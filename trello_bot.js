75c339ce4d051e16511eb057ce06ca6d61230361
ground-taguchi <ground.taguchi@gmail.com>
2024-02-05 13:45:24 +0900

    first commit

---
+++ trello_bot.js
@@ -0,0 +1,65 @@
+// Trello APIキーとトークン
+var trelloApiKey = '';
+var trelloToken = '';
+
+// Line Notifyトークン
+var lineNotifyToken = '';
+
+// TrelloボードID
+var trelloBoardId = '';
+
+function main() {
+  var cards = getNearDueDateCards();
+  if (cards.length > 0) {
+    sendLineNotification(cards);
+  }
+}
+
+function formatDateTime(dateString) {
+  var date = new Date(dateString);
+  var year = date.getFullYear();
+  var month = ('0' + (date.getMonth() + 1)).slice(-2);
+  var day = ('0' + date.getDate()).slice(-2);
+  var hours = ('0' + date.getHours()).slice(-2);
+  var minutes = ('0' + date.getMinutes()).slice(-2);
+  return year + '/' + month + '/' + day + ' ' + hours + ':' + minutes;
+}
+
+function getNearDueDateCards() {
+  var url = 'https://api.trello.com/1/boards/' + trelloBoardId + '/cards?key=' + trelloApiKey + '&token=' + trelloToken;
+  var response = UrlFetchApp.fetch(url);
+  var cards = JSON.parse(response.getContentText());
+
+  // 期限が2日以内のカードを抽出
+  var nearDueDateCards = cards.filter(function(card) {
+    var dueDate = formatDateTime(card.due);
+    var today = new Date();
+    var timeDiff = new Date(dueDate).getTime() - today.getTime();
+    var daysDiff = timeDiff / (1000 * 3600 * 24);
+    return daysDiff <= 2 && daysDiff > 0;
+  });
+
+  return nearDueDateCards;
+}
+
+function sendLineNotification(cards) {
+  var message = '\n期限が近づいてるよ！\n進捗教えてー！\n\n';
+
+  cards.forEach(function(card) {
+    var formattedDueDateTime = formatDateTime(card.due);
+    message += '「' + card.name + '」\n 期限: ' + formattedDueDateTime + 'まで\n\n';
+  });
+
+  var lineUrl = 'https://notify-api.line.me/api/notify';
+  var headers = {
+    'Authorization': 'Bearer ' + lineNotifyToken,
+    'Content-Type': 'application/x-www-form-urlencoded',
+  };
+  var payload = {
+    'method': 'post',
+    'headers': headers,
+    'payload': 'message=' + encodeURIComponent(message),
+  };
+
+  UrlFetchApp.fetch(lineUrl, payload);
+}
