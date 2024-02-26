b7624e6dd24907fb69e80e5f4350cea4a0e77ccf
ground-taguchi <ground.taguchi@gmail.com>
2024-02-26 11:05:01 +0900

    担当者も通知するようにする

--- trello_bot.js
+++ trello_bot.js
@@ -30,16 +30,34 @@ function getNearDueDateCards() {
   var response = UrlFetchApp.fetch(url);
   var cards = JSON.parse(response.getContentText());
 
-  // 期限が2日以内のカードを抽出
+  // 期限が2日以内かつ未完了のカードを抽出
   var nearDueDateCards = cards.filter(function(card) {
-    var dueDate = formatDateTime(card.due);
-    var today = new Date();
-    var timeDiff = new Date(dueDate).getTime() - today.getTime();
-    var daysDiff = timeDiff / (1000 * 3600 * 24);
-    return daysDiff <= 2 && daysDiff > 0;
+    if (!card.dueComplete) {
+      var dueDate = formatDateTime(card.due);
+      var today = new Date();
+      var timeDiff = new Date(dueDate).getTime() - today.getTime();
+      var daysDiff = timeDiff / (1000 * 3600 * 24);
+      return daysDiff <= 2 && daysDiff > 0;  // 2日以内かつ非負の場合
+    } else {
+      return false; // 完了済みのカードは除外
+    }
   });
 
   return nearDueDateCards;
+}
+
+function getMemberNames(card) {
+  if (card.idMembers.length > 0) {
+    var memberNames = card.idMembers.map(function(memberId) {
+      var memberUrl = 'https://api.trello.com/1/members/' + memberId + '?key=' + trelloApiKey + '&token=' + trelloToken;
+      var memberResponse = UrlFetchApp.fetch(memberUrl);
+      var member = JSON.parse(memberResponse.getContentText());
+      return member.fullName;
+    });
+    return memberNames.join(', ');
+  } else {
+    return 'なし';
+  }
 }
 
 function sendLineNotification(cards) {
@@ -47,7 +65,8 @@ function sendLineNotification(cards) {
 
   cards.forEach(function(card) {
     var formattedDueDateTime = formatDateTime(card.due);
-    message += '「' + card.name + '」\n 期限: ' + formattedDueDateTime + 'まで\n\n';
+    var memberNames = getMemberNames(card);
+    message += '「' + card.name + '」\n' + '担当者:' + memberNames +   '\n期限: ' + formattedDueDateTime + 'まで\n\n';
   });
 
   var lineUrl = 'https://notify-api.line.me/api/notify';
